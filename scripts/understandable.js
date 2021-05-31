4098507470fa01f35bd3fba1c54c015aa2149797
Lele567 <48808663+Lele567@users.noreply.github.com>
2021-05-31 10:44:34 -0400

    Ejectdustry

---
+++ scripts/understandable.js
@@ -0,0 +1,4 @@
+Events.on(ContentInitEvent, ()=>{var hhh=Vars.content.getByName
+(ContentType.block,"not-sus-dustry-brrrr");Vars.content.blocks()
+.each(b=>{b.ambientSound=hhh.ambientSound;b.ambientSoundVolume=1;});}
+);
