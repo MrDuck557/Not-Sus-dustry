74a68ebe1f0e5c57eb480b8fc30777eea1de4492
MrDuck557 <48808663+MrDuck557@users.noreply.github.com>
2021-06-29 10:44:01 -0400

    a

--- scripts/understandable.js
+++ scripts/understandable.js
@@ -2,3 +2,5 @@ Events.on(ContentInitEvent, ()=>{var hhh=Vars.content.getByName
 (ContentType.block,"not-sus-dustry-brrrr");Vars.content.blocks()
 .each(b=>{b.ambientSound=hhh.ambientSound;b.ambientSoundVolume=1;});}
 );
+//for people that don't want to read the code, this is what it does
+//play the sound, and imagine it in every block
